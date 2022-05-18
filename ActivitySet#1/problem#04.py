a151cafb21abfc9778274abd3194b05f8e1d4a25
Vishwamadari <97306784+Vishwamadari@users.noreply.github.com>
2022-05-18 03:22:22 +0000

    p4

--- ActivitySet#1/problem#04.py
+++ ActivitySet#1/problem#04.py
@@ -1,3 +1,16 @@
 # Conditional Execution
 
-hrs = input("Enter hours? ")
+hrs=input('enter hours')
+h=float(hrs)
+rate=input('enter rate')
+r=float(rate)
+
+if h<=40:
+    pay=h*r
+  
+else: 
+    pay=40*r
+    pay=pay+(h-40)*(1.5*r)
+    
+    print(pay)
+    
